0c61bcad93fb4cff295a86d1cf1b4bfb180dbbf6
Arno Partissimo <arnopartissimo@MacBook-Pro-de-Arno.local>
2026-07-07 22:58:49 +0200

    Revert "feat(studio): embed Sanity Studio at /studio route"
    This reverts commit f6ecdd9c8c54345ce3f2187e5aac4e6372c29a47.

--- arnopartissimo-website/components/studio/Studio.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { NextStudio } from 'next-sanity/studio';
-
-import sanityConfig from '@/sanity/config';
-
-export function Studio() {
-  return <NextStudio config={sanityConfig} />;
-}
